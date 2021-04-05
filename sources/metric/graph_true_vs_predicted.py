28ec5cee109b35e0c549cb84b3adbdcfc98e8ada
Tom Dymel <td@wps.de>
2021-04-05 09:28:44 +0200

    Implementing metric if something is continouesly correct predicted if in its vincinity of 5 or 10 is the predicted location actually

--- sources/metric/graph_true_vs_predicted.py
+++ sources/metric/graph_true_vs_predicted.py
@@ -44,10 +44,77 @@ class GraphTrueVsPredicted:
             y_predicted_position = [np.asarray(x).argmax() for x in predicted]
 
         accuracy = 0
+        accuracy_given_location_is_cont_the_same_and_within_5_entries = 0
+        accuracy_given_location_is_cont_the_same_and_within_10_entries = 0
         for i in range(len(y_true_position)):
             if y_true_position[i] == y_predicted_position[i]:
                 accuracy = accuracy + 1
+                accuracy_given_location_is_cont_the_same_and_within_5_entries = accuracy_given_location_is_cont_the_same_and_within_5_entries + 1
+                accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries + 1
+            else:
+                is_within_5 = True
+                has_gotten_right_location = False
+                # Check 5 before
+                for j in range(i, max(i - 5, 0) - 1, -1):
+                    is_within_5 = is_within_5 and y_predicted_position[i] == y_predicted_position[j]
+                    has_gotten_right_location = has_gotten_right_location or y_true_position[j] == y_predicted_position[
+                        j]
+                    if has_gotten_right_location:
+                        break
+
+                if has_gotten_right_location and is_within_5:
+                    accuracy_given_location_is_cont_the_same_and_within_5_entries = accuracy_given_location_is_cont_the_same_and_within_5_entries + 1
+                    accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries + 1
+                    continue
+
+                is_within_10 = True
+                has_gotten_right_location = False
+                # Check 10 before
+                for j in range(i, max(i - 10, 0) - 1, -1):
+                    is_within_10 = is_within_10 and y_predicted_position[i] == y_predicted_position[j]
+                    has_gotten_right_location = has_gotten_right_location or y_true_position[j] == y_predicted_position[
+                        j]
+                    if has_gotten_right_location:
+                        break
+
+                if has_gotten_right_location and is_within_10:
+                    accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries + 1
+                    continue
+
+                is_within_5 = True
+                has_gotten_right_location = False
+                # Check 5 after
+                for j in range(i, min(i + 5, len(y_true_position))):
+                    is_within_5 = is_within_5 and y_predicted_position[i] == y_predicted_position[j]
+                    has_gotten_right_location = has_gotten_right_location or y_true_position[j] == y_predicted_position[
+                        j]
+                    if has_gotten_right_location:
+                        break
+
+                if has_gotten_right_location and is_within_5:
+                    accuracy_given_location_is_cont_the_same_and_within_5_entries = accuracy_given_location_is_cont_the_same_and_within_5_entries + 1
+                    accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries + 1
+                    continue
+
+                is_within_10 = True
+                has_gotten_right_location = False
+                # Check 10 after
+                for j in range(i, min(i + 10, len(y_true_position))):
+                    is_within_10 = is_within_10 and y_predicted_position[i] == y_predicted_position[j]
+                    has_gotten_right_location = has_gotten_right_location or y_true_position[j] == y_predicted_position[
+                        j]
+                    if has_gotten_right_location:
+                        break
+
+                if has_gotten_right_location and is_within_10:
+                    accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries + 1
+                    continue
+
         accuracy = accuracy / len(y_true_position)
+        accuracy_given_location_is_cont_the_same_and_within_5_entries = accuracy_given_location_is_cont_the_same_and_within_5_entries / len(
+            y_true_position)
+        accuracy_given_location_is_cont_the_same_and_within_10_entries = accuracy_given_location_is_cont_the_same_and_within_10_entries / len(
+            y_true_position)
 
         accuracy_given_previous_location_was_correct = 0
         total_prev_location_correct = 0
@@ -59,8 +126,12 @@ class GraphTrueVsPredicted:
         accuracy_given_previous_location_was_correct = accuracy_given_previous_location_was_correct / total_prev_location_correct
 
         log_file = open(self.file_path + "log_true_vs_predicted.csv", "w")
-        log_file.write("accuracy,accuracy_given_previous_location_was_correct\n")
-        log_file.write("{0},{1}".format(accuracy, accuracy_given_previous_location_was_correct))
+        log_file.write("accuracy,accuracy_given_previous_location_was_correct,"
+                       "accuracy_given_location_is_cont_the_same_and_within_5_entries,"
+                       "accuracy_given_location_is_cont_the_same_and_within_10_entries\n")
+        log_file.write("{0},{1},{2},{3}".format(accuracy, accuracy_given_previous_location_was_correct,
+                                                accuracy_given_location_is_cont_the_same_and_within_5_entries,
+                                                accuracy_given_location_is_cont_the_same_and_within_10_entries))
         log_file.close()
 
         fig, ax1 = plt.subplots()
