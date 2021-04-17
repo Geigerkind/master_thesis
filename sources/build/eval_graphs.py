Fixing the combined test set calculation

sources/build/eval_graphs.py
@@ -127,6 +127,7 @@ with open("/home/shino/Uni/master_thesis/bin/evaluation_data.pkl", 'rb') as file
         test_sets_knn = []
         test_labels_knn = []
 
+        """
         new_set_dt, new_labels_dt, new_set_knn, new_labels_knn = glue_test_sets(data.anomaly_features_dt[0],
                                                                                 data.anomaly_labels_dt[0],
                                                                                 data.anomaly_features_knn[0],
@@ -155,6 +156,7 @@ with open("/home/shino/Uni/master_thesis/bin/evaluation_data.pkl", 'rb') as file
             test_labels_dt.append(np.asarray(new_labels_dt).copy())
             test_sets_knn.append(np.asarray(new_set_knn).copy())
             test_labels_knn.append(np.asarray(new_labels_knn).copy())
+        """
 
         new_set_dt, new_labels_dt, new_set_knn, new_labels_knn = glue_test_sets(data.test_route_features_dt[0],
                                                                                 data.test_route_labels_dt[0],
@@ -176,11 +178,14 @@ with open("/home/shino/Uni/master_thesis/bin/evaluation_data.pkl", 'rb') as file
             test_sets_knn.append(np.asarray(new_set_knn).copy())
             test_labels_knn.append(np.asarray(new_labels_knn).copy())
 
+        print(test_sets_knn[0])
+
         # test_set_names = ["anomaly", "anomaly2", "simple_square", "long_rectangle", "rectangle_with_ramp", "many_corners", "combination",
         #                  "faulty_permuted_paths", "faulty_nulled_acceleration", "faulty_nulled_light",
         #                  "faulty_nulled_access_point", "faulty_nulled_heading", "faulty_nulled_temperature",
         #                  "faulty_nulled_volume", "faulty_random_acceleration_deviation", "faulty_access_point_random_not_detect"]
-        test_set_names = ["anomaly", "anomaly2", "simple_square", "combination"]
+        #test_set_names = ["anomaly", "anomaly2", "simple_square", "combination"]
+        test_set_names = ["combination"]
         for k in range(len(test_set_names)):
             path = "/home/shino/Uni/master_thesis/bin/main_evaluation/" + test_set_names[k] + "/"
             # Create folder